1b9c05cd46549074be42fc4f793b901830cf39bd
adanaamir <adanaamir2005@gmail.com>
2024-08-22 20:07:37 -0700

    .

--- cs50P final project/project.py
+++ cs50P final project/project.py
@@ -2,17 +2,22 @@ import cowsay, csv
 from random import choice
 import sys, json
 
+cowsay_char = [cowsay.beavis, cowsay.cheese, cowsay.cow, cowsay.daemon, cowsay.dragon, cowsay.fox, cowsay.ghostbusters, 
+               cowsay.kitty,cowsay.meow, cowsay.miki, cowsay.milk, cowsay.octopus, cowsay.stegosaurus, cowsay.stimpy, cowsay.trex, 
+               cowsay.turkey, cowsay.turtle, cowsay.tux]
+
 def main():
     user_choice()
     riddle()
     joke()
     ask_questions()
+    write_questions()
 
 def user_choice():
-    ans = input("Do you wanna hear some jokes or maybe you're feeling clever and wanna solve some riddles? (yes iam / yepp) ").strip().lower()
+    ans = input("Welcome, are you ready to play? (yes iam / yepp) ").strip().lower()
     while True:
         if ans[0] == "y":
-            response = input("\nPlease choose \n1.Riddles, \n2.Jokes \n3.Question asking \n4.Exit the program \n").lower()
+            response = input("\nPlease choose \n1.Riddles \n2.Jokes \n3.Question asking \n4.Exit the program \n").lower()
             
             #calling the functions according to the user response
             if response == "1":
@@ -48,14 +53,22 @@ def user_choice():
 def riddle():
     column= []
     ans = []
+    
     print("\nooo okay! lets see if you can crack this riddle.")
     while True:
+        #clearing out both the lists so that after the loop is run again, the old data is deleted and the lists are being populated correctly
+        column.clear()
+        ans.clear()
+
         with open("before.csv", "r", newline= '') as file:
             csv_reader = csv.DictReader(file)
 
             for line in csv_reader:
                 column.append(line['riddles'])
-            cowsay.fox(choice(column))
+            
+            character = choice(cowsay_char)
+            riddle_choice = choice(column)
+            character(riddle_choice)
                     
             answer = input("Enter your answer: ").lower()
 
@@ -66,29 +79,28 @@ def riddle():
                 for line in csv_reader:
                     ans.append(line['answers'])  #appending the answers to a new list
                         
-                #flag
+                #flag, assuming users answer is incorrect
                 match = False
                 for i in ans:
                     if answer == i:
                         match = True
-                        print("Thats correct!!")
 
-                        response = input("Do you wanna continue solving more riddles? (y/n) ")
+                        response = input("Thats correct!\n\nDo you wanna continue solving more riddles? (y/n) ")
                         if response == "n":
                             user_choice()
                         elif response == "y":
                             print("alright!\n")
-                            response = "0"
+                            continue   #response = "0"
                         else:
                             raise ValueError
 
-                if match == False:  #checking after loop
-                    response = input("Its okay, do you wanna try again? (y/n) ")
+                if not match:  #checking after loop
+                    response = input("incorrect, do you wanna try again? (y/n) ")
                     if response == "n":
                         user_choice()
                     elif response == "y":
                         print("\n\talright!\n")
-                        response = "0"
+                        continue  #skip the rest of the loop and show a new riddle
                     else:
                         raise ValueError
 
@@ -102,7 +114,10 @@ def joke():
 
             for line in csv_reader: 
                 column.append(line['jokes'])
-            cowsay.fox(choice(column))
+
+            character = choice(cowsay_char)  #randomly selecting a character from the character list(global)
+            jokes_choice = choice(column)    #randomly selecting a joke from csv file
+            character(jokes_choice)          #passing the joke to the animal
 
             ans = input("That was funny LOL\nDo you wanna hear some more jokes? (y/n) ")
             if ans == "n":
@@ -116,17 +131,47 @@ def joke():
 
 
 def ask_questions():
-    cowsay.cow("Ask me any question and ill answer it\n If you wanna go to menu then press \"E\" anytime")
+    cowsay.cow("Ask me any question and ill answer it\n If you wanna go to menu then press \"E\" anytime\nPress \"W\" to write responses\n ")
     while True:
-        questions = input()
-        if questions == "E":
-            user_choice()
-            
-        else:
+        try:
+            questions = input()
+            if questions == "E":
+                user_choice()
+            elif questions == "W":
+                write_questions()
+                
+            else:
+                with open("responses.json", "r") as new_file:
+                    reader = json.load(new_file)  #loading the json file
+
+                    response = choice(reader)
+                    character = choice(cowsay_char)
+                    character(response)
+                    
+        except FileNotFoundError:
+            sys.exit()
+
+def write_questions():
+    while True:
+        try:
+            #reading the file before writing to it, since the data already exists in this file
+            questions = input("Enter any hilarious response: ")
+
             with open("responses.json", "r") as new_file:
-                reader = json.load(new_file)  #loading the json file
-            cowsay.cow(choice(reader))
+                data = json.load(new_file)
 
+            data.append(questions)           #appending the users entry into the json file
+            with open("responses.json", "w") as new_file:     #writing the appended data to the file
+                json.dump(data, new_file, indent = 4)
 
+            response = input("Done!\nPress \"M\" if you want to add more , \"E\" to exit: ")
+            if response == "E":
+                user_choice()
+            elif response == "M":
+                continue
+            
+        except FileNotFoundError:
+            sys.exit()
+                
 if __name__ == "__main__":
     main()
