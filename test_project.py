c6cd283ad328a3afaeeb2215655cacdf84cee113
adanaamir <adanaamir2005@gmail.com>
2024-08-23 22:04:21 -0700

    .

--- test_project.py
+++ test_project.py
@@ -1,27 +1,38 @@
-from project import user_choice, riddle, joke, ask_questions, write_questions 
-import sys,pytest
+import pytest
+from project import user_choice
+from unittest.mock import patch
 
-def test_user_choice():
-    assert user_choice("1") == riddle()
-    assert user_choice("2") == joke()
-    assert user_choice("3") == ask_questions
+# Since user_choice() relies on user input, we'll need to mock this input.
+#mocking means instead of typing any values, youre telling python that user has typed this eg "yes" and "1" in this case
+# this was achieved by using patch that replaced the normal input() wiht a fake one that gave python yes and 1 repsonse
 
-    with pytest.raises(ValueError):
-        user_choice("okay")
+def test_user_choice_riddles():
+    with patch("builtins.input", side_effect=["yes", "1"]):
+        try:
+            user_choice()   
+        except Exception as e:  #the actual error message is stored in variable "e"
+            pytest.fail(f"An error occurred: {e}")
+            
+def test_user_choice_jokes():
+    with patch("builtins.input", side_effect=["yes", "2"]):
+        try:
+            user_choice()   
+        except Exception as e:  #the actual error message is stored in variable "e"
+            pytest.fail(f"An error occurred: {e}")
 
-    assert user_choice("e") == sys.exit()
+def test_user_choice_questions():
+    with patch("builtins.input", side_effect=["yes", "3"]):
+        try:
+            user_choice()   
+        except Exception as e:  #the actual error message is stored in variable "e"
+            pytest.fail(f"An error occurred: {e}")
 
-def test_riddle():
-    ...
+def test_user_choice_exit():
+    with patch("builtins.input", side_effect=["no", "1"]):
+        try:
+            user_choice()   
+        except Exception as e:  #the actual error message is stored in variable "e"
+            pytest.fail(f"An error occurred: {e}")
 
 
-def test_joke():
-    ...
 
-
-def test_ask_questions():
-    ...
-
-
-def test_write_questions():
-    ...
